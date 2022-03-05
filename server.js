7711e330b9fc00f9260d897989dcebe82e734e4b
Rafael Seva <rafaelsevaiv@gmail.com>
2022-03-06 01:05:59 +0800

    Added function where correct answer will be given the point/s

--- server.js
+++ server.js
@@ -1,135 +1,196 @@
-require('dotenv').config();
-const axios = require('axios');
-const CronJob = require('cron').CronJob;
-const Discord = require('discord.js');
-const { MessageEmbed } = require('discord.js');
-const JSONdb = require('simple-json-db');
-const db = new JSONdb(__dirname + '/storage.json');
-const { decode } = require('html-entities');
+require("dotenv").config();
+const axios = require("axios");
+const CronJob = require("cron").CronJob;
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const JSONdb = require("simple-json-db");
+const db = new JSONdb(__dirname + "/storage.json");
+const { decode } = require("html-entities");
 
-const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] }); 
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 // logs in console when bot is ready
-client.once('ready', () => {
-    console.log('Trivia For The Day Bot is ready');
-})
+client.once("ready", () => {
+	console.log("Trivia For The Day Bot is ready");
+});
 
 // where there's a message receive and it is equals to the condition, send message pong!
-client.on('messageCreate', message => {  
-    console.log(message); 
-    // trivia for today
-    let answer = db.get('trivia')['correct_answer']; 
-    let incorrect_answer = db.get('trivia')['incorrect_answers'];
-    let question = decode(`${db.get('trivia')['question']}`, {level: 'html5'});
-    let type = db.get('trivia')['type'] == "boolean" ? "True/False" : "Multiple Choice";
-    let category = db.get('trivia')['category'];
-    let difficulty = db.get('trivia')['difficulty'];
-    
-    const exampleEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(question)
-        .addFields(
-            { name: 'Type', value: type, inline: true },
-            { name: 'Category', value: category, inline: true },
-            { name: 'Difficulty', value: difficulty, inline: true },
-        )
+client.on("messageCreate", (message) => {
+	// console.log(message);
+	// trivia for today
+	let trivia = db.get("trivia");
+	let answered = db.get("answered");
 
-    
-    if (db.get('trivia')['type'] == "multiple") {
-        let choices = incorrect_answer;
-        choices.push(answer);
+	let answer = trivia["correct_answer"];
 
-        function fisherYatesShuffle(arr){
-            for(var i =arr.length-1 ; i>0 ;i--){
-                var j = Math.floor( Math.random() * (i + 1) ); //random index
-                [arr[i],arr[j]]=[arr[j],arr[i]]; // swap
-            }
-        }
-        
-        fisherYatesShuffle(choices);
+	// Getting of current trivia
+	if (
+		message.content === "!trivia" &&
+		message.channelId === process.env.CHANNEL_ID
+	) {
+		// Send trivia to discord
+		const exampleEmbed = triviaMessage(trivia);
 
-        exampleEmbed.addField('Take note!', 'Please answer the value, not the choice.', false);
-        exampleEmbed.addFields(
-            { name: 'A', value: choices[0], inline: true },
-            { name: 'B', value: choices[1], inline: true },
-            { name: 'C', value: choices[2], inline: true },
-            { name: 'D', value: choices[3], inline: true },
-        )
-    }
+		client.channels.cache
+			.get(process.env.CHANNEL_ID)
+			.send({ embeds: [exampleEmbed] });
+	}
 
-    // Getting of current trivia
-    if (message.content === "!trivia" && message.channelId === process.env.CHANNEL_ID) {
-        // Send trivia to discord
-        client.channels.cache.get(process.env.CHANNEL_ID).send({ embeds: [exampleEmbed] });
-    }
+	// console.log(answered);
 
-    // Checking if message being sent is equals to the correct answer
-    // Make answer and message lowercase
-    if (message.content.toLowerCase() === answer.toLowerCase()) {
-        console.log('YOU GOT THE CORRECT ANSWER');
-    }
-})
+	// Checking if message being sent is equals to the correct answer
+	// Make answer and message lowercase
+	if (
+		message.content.toLowerCase() === answer.toLowerCase() &&
+		answered === 0
+	) {
+		let user = message.author;
+		let user_id = user.id;
+		let username = user.username;
+
+		let leaderboard = db.get("leaderboard");
+
+		// Check if user is in the leaderboard
+		// If yes, increment points based on point difficulty
+		// If not, add new user object in leaderboard
+		let i = 0;
+		for (; i < leaderboard.length && leaderboard[i]["id"] !== user_id; i++) {}
+
+		if (i !== leaderboard.length) {
+			leaderboard[i]["points"] += trivia.points;
+		} else {
+			leaderboard.push({ id: user_id, points: trivia.points });
+		}
+
+		answered = 1;
+
+		// Update leaderboard and answered indicator
+		db.set("leaderboard", leaderboard);
+		db.set("answered", answered);
+		console.log(leaderboard);
+		console.log(answered);
+		// console.log(message.author);
+		console.log("YOU GOT THE CORRECT ANSWER");
+	} else if (answered === 1) {
+		client.channels.cache
+			.get(process.env.CHANNEL_ID)
+			.send({ embeds: [exampleEmbed] });
+	}
+});
 
 client.login(process.env.DISC_TOKEN);
 
 // UPDATE FOR 8 AM PARAMETERS (0 0 8 * * *)
-let trivia = new CronJob('0 * * * * *', function() {
-    // Get trivia via API
-    // Save trivia via JSON DB
-    // Send trivia to Discord
-    axios.get(process.env.API_CALL)
-    .then(function (response) {
-        // handle success
-        db.set('trivia', response.data.results[0]);
-        let trivia = response.data.results[0];
-        let answer = trivia['correct_answer']; 
-        let incorrect_answer = trivia['incorrect_answers'];
-        let question = decode(`${trivia['question']}`, {level: 'html5'});
-        let type = trivia['type'] == "boolean" ? "True/False" : "Multiple Choice";
-        let category = trivia['category'];
-        let difficulty = trivia['difficulty'];
+let trivia = new CronJob(
+	"0 * * * * *",
+	function () {
+		// Get trivia via API
+		// Save trivia via JSON DB
+		// Send trivia to Discord
+		axios
+			.get(process.env.API_CALL)
+			.then(function (response) {
+				let trivia = response.data.results[0];
 
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(question)
-            .addFields(
-                { name: 'Type', value: type, inline: true },
-                { name: 'Category', value: category, inline: true },
-                { name: 'Difficulty', value: difficulty, inline: true },
-            )
+				trivia["correct_answer"] = decode(`${trivia["correct_answer"]}`, {
+					level: "html5",
+				});
 
-        
-        if (trivia['type'] == "multiple") {
-            let choices = incorrect_answer;
-            choices.push(answer);
+				let difficulty = trivia["difficulty"];
+				let points = 0;
 
-            function fisherYatesShuffle(arr){
-                for(var i =arr.length-1 ; i>0 ;i--){
-                    var j = Math.floor( Math.random() * (i + 1) ); //random index
-                    [arr[i],arr[j]]=[arr[j],arr[i]]; // swap
-                }
-            }
-            
-            fisherYatesShuffle(choices);
+				// Give the points
+				// Check current difficulty
+				switch (difficulty) {
+					case "easy":
+						points = randomPoint([1, 2, 3]);
+						break;
+					case "medium":
+						points = randomPoint([4, 5, 6]);
+						break;
+					case "hard":
+						points = randomPoint([7, 8, 10]);
+						break;
+				}
 
-            exampleEmbed.addField('Take note!', 'Please answer the value, not the choice.', false);
-            exampleEmbed.addFields(
-                { name: 'A', value: choices[0], inline: true },
-                { name: 'B', value: choices[1], inline: true },
-                { name: 'C', value: choices[2], inline: true },
-                { name: 'D', value: choices[3], inline: true },
-            )
-        }
+				// Assign point
+				trivia["points"] = points;
 
-        // Send trivia to discord
-        client.channels.cache.get(process.env.CHANNEL_ID).send({ embeds: [exampleEmbed] });
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
-}, null, true, 'Asia/Manila');
+				// handle success
+				// setting trivia data in db
+				db.set("trivia", trivia);
+				// reset answered state
+				db.set("answered", 0);
+
+				const exampleEmbed = triviaMessage(trivia);
+
+				// Send trivia to discord
+				client.channels.cache
+					.get(process.env.CHANNEL_ID)
+					.send({ embeds: [exampleEmbed] });
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			})
+			.then(function () {
+				// always executed
+			});
+	},
+	null,
+	true,
+	"Asia/Manila"
+);
 trivia.start();
+
+// OTHER FUNCTIONS
+function randomPoint(points) {
+	return points[Math.floor(Math.random() * points.length)];
+}
+
+function triviaMessage(trivia) {
+	let answer = trivia["correct_answer"];
+	let incorrect_answer = trivia["incorrect_answers"];
+	let question = decode(`${trivia["question"]}`, { level: "html5" });
+	let type = trivia["type"] == "boolean" ? "True/False" : "Multiple Choice";
+	let category = trivia["category"];
+	let difficulty = trivia["difficulty"];
+	let points = trivia["points"];
+
+	const exampleEmbed = new MessageEmbed()
+		.setColor("#0099ff")
+		.setTitle(question + ` (${points} pts)`)
+		.addFields(
+			{ name: "Type", value: type, inline: true },
+			{ name: "Category", value: category, inline: true },
+			{ name: "Difficulty", value: difficulty, inline: true }
+		);
+
+	if (trivia["type"] == "multiple") {
+		let choices = incorrect_answer;
+		choices.push(answer);
+
+		function fisherYatesShuffle(arr) {
+			for (var i = arr.length - 1; i > 0; i--) {
+				var j = Math.floor(Math.random() * (i + 1)); //random index
+				[arr[i], arr[j]] = [arr[j], arr[i]]; // swap
+			}
+		}
+
+		fisherYatesShuffle(choices);
+
+		exampleEmbed.addField(
+			"Take note!",
+			"Please answer the value, not the choice.",
+			false
+		);
+		exampleEmbed.addFields(
+			{ name: "A", value: choices[0], inline: true },
+			{ name: "B", value: choices[1], inline: true },
+			{ name: "C", value: choices[2], inline: true },
+			{ name: "D", value: choices[3], inline: true }
+		);
+	}
+
+	return exampleEmbed;
+}
